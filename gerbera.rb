d244f93377d6c79ed60cf35d344fe0d96575b777
Ian Whyman <ian@gerbera.io>
2021-03-01 10:06:37 +0000

    Bump to 1.7.0 (#26)

--- gerbera.rb
+++ gerbera.rb
@@ -1,8 +1,8 @@
 class Gerbera < Formula
-  desc "UPnP Media Server for 2020"
+  desc "UPnP Media Server for 2021"
   homepage "https://gerbera.io"
-  url "https://github.com/gerbera/gerbera/archive/v1.6.1.tar.gz"
-  sha256 "9bb119ee422c8e7a053c309256215a8899dfd6707f6b93496e6facea7978ac7c"
+  url "https://github.com/gerbera/gerbera/archive/v1.7.0.tar.gz"
+  sha256 "6d40b840d015ba5c512f41442957c1c0fdbba07c18724cf00a45e0a579ddeb7f"
 
   depends_on "cmake" => :build
   depends_on "duktape"
@@ -14,24 +14,26 @@ class Gerbera < Formula
   depends_on "libupnp"
   depends_on "lzlib"
   depends_on "ossp-uuid"
+  depends_on "pugixml"
+  depends_on "spdlog"
   depends_on "taglib"
 
   def install
     mkdir "build" do
       args = std_cmake_args
-      args << "-DWITH_CURL=1"
-      args << "-DWITH_JS=1"
-      args << "-DWITH_TAGLIB=1"
-      args << "-DWITH_AVCODEC=1"
-      args << "-DWITH_EXIF=1"
-      args << "-DWITH_SYSTEMD=0"
-      args << "-DWITH_INOTIFY=0"
+      args << "-DWITH_CURL=YES"
+      args << "-DWITH_JS=YES"
+      args << "-DWITH_TAGLIB=YES"
+      args << "-DWITH_AVCODEC=YES"
+      args << "-DWITH_EXIF=YES"
+      args << "-DWITH_SYSTEMD=NO"
+      args << "-DWITH_INOTIFY=NO"
+      args << "-DWITH_MYSQL=NO"
+      args << "-DWITH_FFMPEGTHUMBNAILER=YES"
       args << "-DCMAKE_FIND_FRAMEWORK=LAST"
       args << "-DCMAKE_CXX_FLAGS=\"-stdlib=libc++\""
       args << "-DCMAKE_CXX_COMPILER=/usr/bin/clang++"
       args << "-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}"
-      args << "-DWITH_FFMPEGTHUMBNAILER=1"
-      args << "-DWITH_MYSQL=0"
 
       system "cmake", "..", *args
       system "make", "install"
@@ -39,6 +41,6 @@ class Gerbera < Formula
   end
 
   test do
-    assert_match /Gerbera UPnP Server/, shell_output("#{bin}/gerbera --compile-info").strip
+    assert_match(/Gerbera UPnP Server/, shell_output("#{bin}/gerbera --compile-info").strip)
   end
 end
