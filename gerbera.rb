20550ef475f670de5146520621bcb4c61fbb4bc8
Karlchen <k_straussberger@netzland.net>
2024-10-04 23:28:13 +0200

    Update gerbera.rb

--- gerbera.rb
+++ gerbera.rb
@@ -1,7 +1,7 @@
 class Gerbera < Formula
   desc "UPnP Media Server for 2024"
   homepage "https://gerbera.io"
-  url "https://github.com/gerbera/gerbera/refs/tags/v2.3.0"
+  url "https://github.com/gerbera/gerbera/releases/tag/v2.3.0"
   sha256 "c9f7956d9e94a9b8ded7de809ba8496ad1a2279b624ab121652dff2a28a1a69e"
 
   depends_on "cmake" => :build
