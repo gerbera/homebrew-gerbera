bc5925b78153174e088a22026d2e7baff0bcd127
Karlchen <k_straussberger@netzland.net>
2026-01-16 08:01:56 +0100

    Update Gerbera to version 3.1.0

--- gerbera.rb
+++ gerbera.rb
@@ -1,8 +1,8 @@
 class Gerbera < Formula
   desc "UPnP Media Server for 2025"
   homepage "https://gerbera.io"
-  url "https://github.com/gerbera/gerbera/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "d7934a2318f45330deb2ed1b589bd37b49f42f7929a9d11650349d17c998f920"
+  url "https://github.com/gerbera/gerbera/archive/refs/tags/v3.1.0.tar.gz"
+  sha256 "9adbe661f24cb42f0f9572ed4496bf2e27ee79d536442cdcbb712177e565af2d"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
